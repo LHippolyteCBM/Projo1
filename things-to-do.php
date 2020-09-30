c6a88389367b04b5a3fc1327e57c7bc2cdae6e0d
JulietteCoetmeur <43242520+JulietteCoetmeur@users.noreply.github.com>
2020-09-30 16:25:47 +0200

    first version of things to do page

--- things-to-do.php
+++ things-to-do.php
@@ -7,7 +7,36 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <?php include 'header.php';?>
+    <header>
+        <?php include 'header.php';?>
+        <div class="banner-things">
+            <h1>Things to do</h1>
+        </div>
+    </header>
+
+    <main>
+        <div class="introduction">
+            <h2>Top activities</h2>
+            <p>Planet Earth offers many super activities Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="card">
+            <img src="" alt="">
+            <h3>Radioactive Eiffel Tower</h3>
+            <p>Planet Earth offers many super activities Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="card">
+            <img src="" alt="">
+            <h3>Special burger place</h3>
+            <p>Planet Earth offers many super activities Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="card">
+            <img src="" alt="">
+            <h3>Human museum</h3>
+            <p>Planet Earth offers many super activities Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+    </main>
+
+
     <?php include 'footer.php';?>
     <script src="main.js"></script>
 
