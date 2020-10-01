74c4e898681640a8fe64a36dba8b71d67da068d7
blazius33 <benoitfinet@wanadoo.fr>
2020-10-01 14:42:39 +0200

    premiers essais

---
+++ footer.php
@@ -0,0 +1,18 @@
+<footer class="footer">
+    <div>
+        Our favorites networks
+    </div>
+    <div>
+    <ul>
+        <li><a href="https://fr-fr.facebook.com/WildCodeSchool/"target ="_blank"><img class="imgfb" src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Facebook_logo_36x36.svg/600px-Facebook_logo_36x36.svg.png"></a></li>
+        <li><a href="https://www.youtube.com/channel/UCi99G_0QPx5sYsK8zdvQzfw" target ="_blank"><img src ="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTYfKnLDqsenV6fQXqW2oMsSpEXFyNbGNjHEw&usqp=CAU"></a></li>
+        <li><a href="https://twitter.com/wildcodeschool" target ="_blank"><img src ="https://ressources.blogdumoderateur.com/2019/11/twitter-logo-1200x1200.jpg"></a></li>
+        <li><a href="https://www.instagram.com/wildcodeschoolofficial/?hl=fr" target ="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/langfr-220px-Instagram_logo_2016.svg.png"></a></li>
+        <li><a href="https://www.linkedin.com/school/wild-codes-chool/" target ="_blank"><img src="https://i2.wp.com/guillaumekasbarian.fr/wp-content/uploads/2019/01/linkedin-logo-high-res-1254-1024x1024.jpg?ssl=1"></a></li>
+        <li><a href="https://github.com/WildCodeSchool" target ="_blank"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Octicons-mark-github.svg/220px-Octicons-mark-github.svg.png"></a></li>
+    </ul>
+    </div>
+    <div>
+        <p>Student team first project at <a href="https://www.wildcodeschool.com/fr-FR" target ="_blank">Wild Code School Paris</a></p>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -5,11 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" media="screen and (min-width: 300px) and (max-width: 730px)" href="style.css" type="text/css"/>
+    <link rel="stylesheet" media="screen and (min-width: 731px)" href="style.css" type="text/css"/>
+    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" href="style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Monoton&display=swap" href="style.css" rel="stylesheet">
 </head>
 <body>
     <?php include 'header.php';?>
-    <?php include 'footer.php';?>
     <script src="main.js"></script>
-
+    <?php include 'footer.php';?>
 </body>
 </html>
