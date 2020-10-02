f6343fb9ab0d3b543eb95002ecfe99990d98c60d
blazius33 <benoitfinet@wanadoo.fr>
2020-10-02 13:54:31 +0200

    maj

--- accommodation.php
+++ accommodation.php
@@ -10,11 +10,46 @@
 
     <header>
         <?php include "header.php" ?>
-        <div class="banniere"></div>
+        <div class="banniereaccommodation"></div>
     </header>
     <main>
         <div class="global-card-container">
-            
+            <div class="card">
+                <div class="imgBx">
+                    <img src="img/one.svg">
+                </div>
+                <div class="content">
+                    <h2>Card One</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore dolorum quod saepe sunt asperiores autem dignissimos ab, hic eaque. Dolorum doloremque quas repellendus tenetur odio pariatur dicta error assumenda nemo?</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="imgBx">
+                    <img src="img/two.svg">
+                </div>
+                <div class="content">
+                    <h2>Card Two</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore dolorum quod saepe sunt asperiores autem dignissimos ab, hic eaque. Dolorum doloremque quas repellendus tenetur odio pariatur dicta error assumenda nemo?</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="imgBx">
+                    <img src="img/three.svg">
+                </div>
+                <div class="content">
+                    <h2>Card Three</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore dolorum quod saepe sunt asperiores autem dignissimos ab, hic eaque. Dolorum doloremque quas repellendus tenetur odio pariatur dicta error assumenda nemo?</p>
+                </div>
+            </div>
+            <div class="card">
+                <div class="imgBx">
+                    <img src="img/four.svg">
+                </div>
+                <div class="content">
+                    <h2>Card Four</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore dolorum quod saepe sunt asperiores autem dignissimos ab, hic eaque. Dolorum doloremque quas repellendus tenetur odio pariatur dicta error assumenda nemo?</p>
+                </div>
+            </div>
         </div>
     </main>
     <?php include 'burger.php'; ?>
