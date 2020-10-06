3b4b3985a39d32800b9b7372726b64ba512fbddc
jeromenicco <jeromenicco36@gmail.com>
2020-10-06 11:48:02 +0200

    changing branch with current change to continue on nav

--- header.php
+++ header.php
@@ -2,13 +2,13 @@
 <nav>
   <div class="banniere">
     <ul class="nav-container">
-      <li><a class="nav-items">HOME</a></li>
-      <li><a class="nav-items">ACTIVITIES</a></li>
-      <li><a class="nav-items">ACCOMODATION</a></li>
-      <li><a class="nav-items">FIND US</a></li>
-      <li><a class="nav-items">ONLINE BOOKING</a></li>
+      <li><a href="./index.php" class="nav-items">HOME</a></li>
+      <li><a href="./things-to-do.php" class="nav-items">ACTIVITIES</a></li>
+      <li><a href="./accommodation.php" class="nav-items">ACCOMODATION</a></li>
+      <li><a href="./traveller.php" class="nav-items">FIND US</a></li>
+      <li><a href="./booking.php" class="nav-items">ONLINE BOOKING</a></li>
     </ul>
   </div>
 </nav>
 
-<script src="navbar.js"></script>
+<script src="navbar.js" type="script"></script>
--- navbar.js
+++ navbar.js
@@ -1,6 +1,19 @@
-const navItems = document.querySelector('.nav-items');
+//const navItems = document.querySelector('.nav-items');
 
 
-navItems.addEventListener('click', () => {
-navItems.classList.toggle("focusOnclick");
-})
+//navItems.addEventListener('click', () => {
+//navItems.classList.toggle("focusOnclick");
+//})
+
+
+
+
+//active nav items ---->
+
+const currentLocation = Location.href;
+const menuItem = document.querySelectorAll('.nav-items');
+    for (let i = 0; i < menuItem.length; i++){
+        if(menuItem[i].href === currentLocation){
+            menuItem[i].classList.toggle("active");
+        }
+    }
