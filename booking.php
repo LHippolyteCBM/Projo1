f8d048fa9852f5febc65474dc1dac5cf051a254e
julien willette <julien.willette@gmail.com>
2020-10-06 15:51:02 +0200

    html fini, css non responsive

--- booking.php
+++ booking.php
@@ -15,42 +15,94 @@
     </header>
     
 
-    <main class="container-all">
-        <h2 class="booking-h1">Booking</h2>
-        <div class="container-form">
-            <div class="block-left-form">
-                <label>
-                    <br><input placeholder="Destination" type="text" name="name"/> <br>
-                </label>
-                <label>
-                    <br> <input placeholder="Duration" type="text" name="Duration"/> <br>
-                </label>
-                <label>
-                    <br><input placeholder="Departure date" type="text" name="Departur"/><br>
-                </label>
+    <main>
+        <div class="container-all">
+            <h2>Booking</h2>
+            <div class="container-form-1">
+                <div class="block-left-form-1">
+                    <label>
+                        <br><input placeholder="Destination" type="text" name="name" class="case"/> <br>
+                    </label>
+                    <label>
+                        <br> <input placeholder="Duration" type="text" name="Duration" class="case"/> <br>
+                    </label>
+                    <label>
+                        <br><input placeholder="Departure date" type="text" name="Departur" class="case"/><br>
+                    </label>
+                </div>
+                <div class="block-right-form-1">
+                    <label>
+                        <br><input placeholder="Budget" type="text" name="Buget" class="case"/><br>
+                    </label>
+                    <label>
+                        <br><input placeholder="Number of adults" type="text" name="Number of adults" class="case"/><br>
+                    </label>
+                    <label>
+                        <br><input placeholder="Number of childrens" type="text" name="Number of children" class="case"/><br>
+                    </label>
+                </div>
             </div>
-            <div class="block-right-form">
-                <label>
-                    <br><input placeholder="Budget" type="text" name="Buget"/><br>
-                </label>
-                <label>
-                    <br><input placeholder="Number of adults" type="text"" name="Number of adults"/><br>
-                </label>
-                <label>
-                    <br><input placeholder="Number of childrens" type="text" name="Number of children"/><br>
-                </label>
+            <div class="container-form-2">
+                <div class="block-left-form-2">
+                     <label for="Civility"></label>
+                        <select name="Civility" id="Civility-select" class="civility">
+                            <option value="">--Please choose a civility--</option>
+                            <option value="dog">M.</option>
+                            <option value="cat">MME.</option>
+                            <option value="hamster">Robot.</option>
+                            <option value="others">Others.</option>
+                        </select>
+                    <label>
+                        <br> <input placeholder="First name" type="text" name="Duration" class="case"/> <br>
+                    </label>
+                    <label>
+                        <br><input placeholder="Adress" type="text" name="Departur" class="case"/><br>
+                    </label>
+                </div>
+                <div class="block-right-form-2">
+                    <label>
+                        <br><input placeholder="Email" type="text" name="Buget" class="case"/><br>
+                    </label>
+                    <label>
+                        <br><input placeholder="Second name" type="text" name="Number of adults" class="case"/><br>
+                    </label>
+                    <label>
+                        <br><input placeholder="Country" type="text" name="Number of children" class="case"/><br>
+                    </label>
+                </div>
             </div>
+                <div class="container-form-3">
+                    <div class="block-cont-form">
+                        <label>
+                        <br><input placeholder="Comment" type="text" name="Comment" class="comment" rows="10" cols="60"/><br>
+                        </label>
+                    </div>
+                    <div class="checkbox-container">
+                    <div class="checkbox">
+                        <input type="checkbox" name="I participate in the trip" value="true" id="je participe au voyage">
+                        <label for="I participate in the trip">I participate in the trip</label>
+                    </div>
+                    
+
+                        <div class="checkbox">
+                            <input type="checkbox" name="I accept the general conditions" value="true" id="je participe au voyage">
+                            <label for="I accept the general conditions">I accept the general conditions</label>
+                        </div>
+                        <div class="checkbox">
+                            <input type="checkbox" name="I agree to be contacted by email" value="true" id="je participe au voyage">
+                            <label for="I agree to be contacted by email">I agree to be contacted by email</label>
+                        </div>
+
+                        <div class="send-input-container">
+                            <label>
+                                <input type="submit" value="Book" class="send"/>
+                            </label>  
+                        </div>
+                    </div>
+                </div>
+
+
         </div>
-        <div class="block-cont-form">
-                <label>
-                    <br><textarea placeholder="Comments" type="text" name="Comments"  rows="10" cols="60" > </textarea><br>
-                </label>
-        </div>
-        <div class="send-input-container">
-                <label>
-                    <input type="submit" value="Send" class="send"/>
-                </label>
-            </div>
     </main>
 
 
