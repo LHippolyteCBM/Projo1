54f6e6811062cfeab5af9154aa8e7f306ae66ead
romheloise <romheloise@gmail.com>
2020-10-07 13:53:27 +0200

    add variable to css

--- nav.js
+++ nav.js
@@ -15,28 +15,30 @@ window.addEventListener('scroll', menuTrans);
 
 
 function menuTrans() {
-    
-    let testExist = backMove.childNodes.length > 0;
-    let navBlocBottom = navBloc.offsetTop + navBloc.offsetHeight;
 
-    tempDir = parseInt(backMove.style.height);
+    if (window.getComputedStyle(navBloc).display === 'flex') {
+        let testExist = backMove.childNodes.length > 0;
+        let navBlocBottom = navBloc.offsetTop + navBloc.offsetHeight;
 
-    if (parseInt(backMove.style.height) >= navBloc.offsetHeight && scrollY > navBlocBottom) {
-        backMove.style.height = navBloc.offsetHeight + 'px';
-        if (!testExist) backMove.appendChild(cloneMenu);
-
-    } else if (scrollY > navBloc.offsetTop) {
-        backMove.style.height = ((window.scrollY - navBloc.offsetTop) / 2) + 'px';
-        parseInt(backMove.style.height) > tempDir ? backDirection = 'DOWN' : backDirection = 'UP';
         tempDir = parseInt(backMove.style.height);
 
-    } else if (scrollY < navBloc.offsetTop) {
-        backMove.style.height = 0;
-    }
+        if (parseInt(backMove.style.height) >= navBloc.offsetHeight && scrollY > navBlocBottom) {
+            backMove.style.height = navBloc.offsetHeight + 'px';
+            if (!testExist) backMove.appendChild(cloneMenu);
+
+        } else if (scrollY > navBloc.offsetTop) {
+            backMove.style.height = ((window.scrollY - navBloc.offsetTop) / 2) + 'px';
+            parseInt(backMove.style.height) > tempDir ? backDirection = 'DOWN' : backDirection = 'UP';
+            tempDir = parseInt(backMove.style.height);
+
+        } else if (scrollY < navBloc.offsetTop) {
+            backMove.style.height = 0;
+        }
 
 
-    if (testExist && backDirection === 'UP') {
-        backMove.removeChild(backMove.childNodes[0]);
+        if (testExist && backDirection === 'UP') {
+            backMove.removeChild(backMove.childNodes[0]);
+        }
     }
 
 }
